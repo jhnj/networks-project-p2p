33d52a51e8f237087c0eacdbc927ab64d4b30344
Walter Berggren <walter.berggren@gmail.com>
2017-03-18 16:42:44 +0200

    Add TrackerServer class

---
+++ tracker/src/main/java/TrackerServer.java
@@ -0,0 +1,13 @@
+import java.net.ServerSocket;
+
+/**
+ * Created by walter on 2017-03-18.
+ */
+public class TrackerServer {
+    int port;
+
+    public TrackerServer(int port) {
+        this.port = port;
+    }
+
+}
