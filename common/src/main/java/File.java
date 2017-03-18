2bda7dc7842b009a05907804a37861951d12bbfe
Walter Berggren <walter.berggren@gmail.com>
2017-03-18 17:53:53 +0200

    Make instance variables private and use getter methods 😎

--- common/src/main/java/File.java
+++ common/src/main/java/File.java
@@ -2,15 +2,31 @@
  * Created by walter on 2017-03-18.
  */
 public class File {
-    String name;
-    int size; //File size in bytes
-    int hash;
-    String[] blocks;
+    private String name;
+    private int size; //File size in bytes
+    private int hash;
+    private String[] blocks;
 
     public File(String name, int size, int hash, String[] blocks) {
         this.name = name;
         this.size = size;
         this.hash = hash;
         this.blocks = blocks;
+    }
+
+    public String[] getBlocks() {
+        return blocks;
+    }
+
+    public int getHash() {
+        return hash;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
     }
 }
