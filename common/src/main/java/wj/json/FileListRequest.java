bf349c444c7a9b6e039f42bbb9d400ad4f573c88
Walter Berggren <walter.berggren@gmail.com>
2017-03-18 21:56:11 +0200

    Add FileListRequest and parser for it

---
+++ common/src/main/java/wj/json/FileListRequest.java
@@ -0,0 +1,25 @@
+package wj.json;
+
+/**
+ * Created by walter on 2017-03-18.
+ */
+public class FileListRequest {
+    private String action;
+    private String[] existing_files;
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String[] getExisting_files() {
+        return existing_files;
+    }
+
+    public void setExisting_files(String[] existing_files) {
+        this.existing_files = existing_files;
+    }
+}
---
+++ common/src/main/java/wj/json/MessageParser.java
@@ -0,0 +1,28 @@
+package wj.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.IOException;
+
+/**
+ * Created by walter on 2017-03-18.
+ */
+public class MessageParser {
+
+    public static String getAction(String jsonString) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.readValue(jsonString, ObjectNode.class);
+        if (objectNode.has("action")) {
+            return objectNode.get("action").asText();
+        } else {
+            return "";
+        }
+    }
+
+    public static FileListRequest parseFileListRequest(String jsonString) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(jsonString, FileListRequest.class);
+    }
+
+}
