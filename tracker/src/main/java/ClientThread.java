ab1fcfe35a625b1e05d43dd05d84cc897e9d447b
Walter Berggren <walter.berggren@gmail.com>
2017-03-18 19:39:38 +0200

    Print requests of JSON type to the console

--- tracker/src/main/java/ClientThread.java
+++ tracker/src/main/java/ClientThread.java
@@ -1,15 +1,56 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class ClientThread implements Runnable {
     private TrackerServer server;
     private Socket socket;
+    private InputStream input;
+    private WJReader reader;
 
     ClientThread(TrackerServer server, Socket socket) {
         this.server = server;
         this.socket = socket;
+
+        try {
+            this.input = socket.getInputStream();
+        } catch (IOException e) {
+            System.err.println(e);
+            this.close();
+        }
+
+        this.reader = new WJReader(this.input);
     }
 
+    public void run() {
+        while (true) {
+            WJType resultType = this.reader.getResultType();
 
-    public void run() {}
+            switch (resultType) {
+                case JSON:
+                    this.handleJSONRequest();
+                default:
+                    System.err.println("Non-JSON request from client");
+                    break;
+            }
+        }
+    }
+
+    private void handleJSONRequest() {
+        try {
+            String jsonString = this.reader.getJSON();
+            System.out.println(jsonString);
+        } catch (WJException e) {
+            System.out.println("Error while retrieving JSON string: " + e.getMessage());
+        }
+    }
+
+    /** Closes the connection and removes the user from any files it is associated to.
+     *
+     *  Called when the pipe is broken.
+     */
+    private void close() {
+        //TODO
+    }
 
 }
