dce2d78f0e6de4e48d0ddc52fb61f2a83b7aa831
Walter Berggren <walter.berggren@gmail.com>
2017-03-18 18:07:56 +0200

    Store files in the TrackerServer, methods for getting files and adding them

--- tracker/src/main/java/TrackerServer.java
+++ tracker/src/main/java/TrackerServer.java
@@ -1,14 +1,18 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by walter on 2017-03-18.
  */
 public class TrackerServer {
     ServerSocket serverSocket;
+    private CopyOnWriteArrayList<File> files;
 
     public TrackerServer(int port) {
+        this.files = new CopyOnWriteArrayList<>();
         try {
             this.serverSocket = new ServerSocket(port);
         } catch (IOException e) {
@@ -27,5 +31,13 @@ public class TrackerServer {
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    public void addFile(File file) {
+        this.files.add(file);
+    }
+
+    public Iterator<File> getFilesIterator() {
+        return this.files.iterator();
     }
 }
