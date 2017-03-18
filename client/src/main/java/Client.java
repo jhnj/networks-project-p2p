bf7a9dfa913830fddc6a61d86886a36f31fadb77
Johan Jern <johanjern95@gmail.com>
2017-03-18 23:00:54 +0200

    Cli interface for client

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -2,41 +2,58 @@
  * Created by johan on 17/03/17.
  *
  */
+import wj.json.WJFile;
 
-
-import org.joda.time.LocalTime;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Map;
 
 public class Client {
+    Map<WJFile, String> files;
+
     public static void main(String[] args) {
-        LocalTime currentTime = new LocalTime();
-        System.out.println("The current local time is: " + currentTime);
-
         try {
-            Socket socket = new Socket("localhost", 3004);
-            OutputStream out = socket.getOutputStream();
-            String jsonString = "{ \"action\": \"file_list\", \"existing_files\": [\"Test\"] }";
-            byte[] data = new byte[3 + jsonString.length()];
-            data[0] = (byte) 0x00;
-            data[1] = (byte) 0x00;
-            data[2] = (byte) jsonString.length();
-            int i = 3;
-            for (char ch: jsonString.toCharArray()) {
-                data[i] = (byte) ch;
-                i++;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Choose port: ");
+            int port = Integer.parseInt(reader.readLine());
+            System.out.print("Enter tracker ip:");
+            String tracker = reader.readLine();
+
+            Socket socket = new Socket(tracker, 3004, InetAddress.getByName("localhost"), port);
+
+            while (true) {
+                System.out.println("Press A to add Files and D to download files");
+                String action = reader.readLine();
+
+                switch (action) {
+                    case "A":
+                        OutputStream out = socket.getOutputStream();
+                        String jsonString = "{ \"action\": \"file_list\", \"existing_files\": [\"Test\"] }";
+                        byte[] data = new byte[3 + jsonString.length()];
+                        data[0] = (byte) 0x00;
+                        data[1] = (byte) 0x00;
+                        data[2] = (byte) jsonString.length();
+                        int i = 3;
+                        for (char ch : jsonString.toCharArray()) {
+                            data[i] = (byte) ch;
+                            i++;
+                        }
+
+                        out.write(data);
+                        out.flush();
+                        break;
+                    case "D":
+                    default:
+                }
             }
-
-            out.write(data);
-            out.flush();
-            out.close();
-
-        } catch (IOException ioException) {
-            System.err.println("IOException: " + ioException.getMessage());
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("NumberFormatException: " + e.getMessage());
         }
 
     }
