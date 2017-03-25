5df1f65acc0787008168f1af73248078e5e45748
Johan Jern <johanjern95@gmail.com>
2017-03-25 12:56:17 +0200

    fixed merge

--- client/src/main/java/FileProvider.java
+++ client/src/main/java/FileProvider.java
@@ -25,8 +25,7 @@ public class FileProvider implements Runnable {
         while (true) {
             try {
                 Socket socket = this.serverSocket.accept();
-                new Thread(new FileProviderThread(socket)).start();
-                new FileProviderThread(socket, fileHandler).run();
+                new Thread(new FileProviderThread(socket, fileHandler)).start();
             } catch (IOException e) {
                 System.out.println("IOException in FileProvider: " + e.getMessage());
             }
